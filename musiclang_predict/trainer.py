156adb7c63e5ec37f2938ba1383227c9c42b8d52
floriangardin <fgardin.pro@gmail.com>
2023-02-14 16:59:55 +0100

    modify readme

--- musiclang_predict/trainer.py
+++ musiclang_predict/trainer.py
@@ -1,5 +1,5 @@
 from .train_utils import train
-from .configs import CONFIG_MUSICLANG_BASIC
+from .configs import CONFIG_MUSICLANG_BASIC, CONFIG_MUSICLANG_BASIC_SMALL
 
 
 
@@ -8,6 +8,18 @@ class MusicLangBasicTrainer:
     def train(self, out_dir, dataset_dir, **config):
 
         base_config = dict(CONFIG_MUSICLANG_BASIC)
+        base_config.update(**config)
+        base_config.update(out_dir=out_dir)
+        base_config.update(dataset=dataset_dir)
+
+        train(**base_config)
+
+
+class MusicLangBasicTrainerXs:
+
+    def train(self, out_dir, dataset_dir, **config):
+
+        base_config = dict(CONFIG_MUSICLANG_BASIC_SMALL)
         base_config.update(**config)
         base_config.update(out_dir=out_dir)
         base_config.update(dataset=dataset_dir)
