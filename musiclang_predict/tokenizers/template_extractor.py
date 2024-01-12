a318764703bca24e9d8bca25961843a22f9889cd
floriangardin <fgardin.pro@gmail.com>
2024-01-12 17:44:50 +0100

    add score to template

--- musiclang_predict/tokenizers/template_extractor.py
+++ musiclang_predict/tokenizers/template_extractor.py
@@ -12,6 +12,21 @@ def midi_file_to_template(midi_file, chord_range=None, max_instruments=8, quanti
     :return: dict, template
     """
     score_prompt = Score.from_midi(midi_file, quantization=quantization, chord_range=chord_range)
+    return score_to_template(score_prompt, max_instruments=max_instruments)
+
+
+def score_to_template(score, max_instruments=8):
+    """
+    Extract a song template from a musiclang score. It will extract the chord progression, the orchestration,
+    The average density, the average amplitude, the average octave for each instrument of each bar.
+
+    It will also extract metadata about the soundtrack like the tonality, the tempo and the time signature.
+    :param score: str, path to midi file
+    :param chord_range: tuple, range of chords to extract (start, end) (default=None)
+    :param max_instruments: int, maximum number of instruments to extract (default=8)
+    :return: dict, template
+    """
+    score_prompt = score.to_score()
     densities_per_chords = [chord.to_score().extract_densities() for chord in score_prompt]
     amplitudes_per_chords = [chord.to_score().extract_mean_amplitudes() for chord in score_prompt]
     octaves_per_chords = [chord.to_score().extract_mean_octaves() for chord in score_prompt]
