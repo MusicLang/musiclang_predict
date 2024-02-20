d5361d9f38dac9f877e983daeb8daf17a897fe0f
floriangardin <fgardin.pro@gmail.com>
2024-02-20 12:04:05 +0100

    add max token length for bpe

--- musiclang_predict/tokenizers/tokenizer.py
+++ musiclang_predict/tokenizers/tokenizer.py
@@ -429,7 +429,7 @@ class MusicLangTokenizer:
         tokenizer = Tokenizer(models.BPE())
 
         # Initialize the BPE trainer
-        trainer = trainers.BpeTrainer(vocab_size=vocab_size, show_progress=True)
+        trainer = trainers.BpeTrainer(vocab_size=vocab_size, show_progress=True, max_token_length=32)
 
         # Train the tokenizer using the BPEIterator
         tokenizer.train_from_iterator(bpe_iterator, trainer=trainer)
