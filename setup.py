bfb9c18be7204e9e638ca583bd0baa8e39dd1f8b
floriangardin <fgardin.pro@gmail.com>
2024-03-20 17:43:28 +0100

    fast inference on cpu

--- setup.py
+++ setup.py
@@ -1,45 +1,13 @@
 import setuptools
 from setuptools import setup, Extension, find_packages
-from setuptools.command.install import install
-from setuptools.command.install_lib import install_lib
-from setuptools.command.build_clib import build_clib
 from setuptools.command.build_py import build_py
-from setuptools.command.build_ext import build_ext
-from setuptools.command.develop import develop
-from setuptools.command.egg_info import egg_info
 import subprocess
+from pathlib import Path
 import os
 
 def custom_command():
     subprocess.check_call(['make', '-C', 'musiclang_predict/c/'])
 
-# Define the C extension
-class CustomInstall(install):
-    """Custom build command that runs a Makefile."""
-
-    def run(self):
-        print('is called install')
-        custom_command()
-        # Call the superclass methods to handle Python extension building, if any
-        install.run(self)
-
-class CustomInstallClib(build_clib):
-    """Custom build command that runs a Makefile."""
-
-    def run(self):
-        print('is called install')
-        custom_command()
-        # Call the superclass methods to handle Python extension building, if any
-        build_clib.run(self)
-
-class CustomInstallExt(install_lib):
-    """Custom build command that runs a Makefile."""
-
-    def run(self):
-        print('is called install')
-        custom_command()
-        # Call the superclass methods to handle Python extension building, if any
-        install_lib.run(self)
 
 class CustomInstallBuildPy(build_py):
     """Custom build command that runs a Makefile."""
@@ -51,25 +19,18 @@ class CustomInstallBuildPy(build_py):
         build_py.run(self)
 
 
-class CustomEggInfoCommand(egg_info):
-    def run(self):
-        print('is called egg')
-        custom_command()
-        egg_info.run(self)
-
-
 module = Extension('musiclang_predict.c',
                    sources=['musiclang_predict/c/run.c'],
                    include_dirs=[],
                    extra_compile_args=['-Ofast', '-fPIC', '-shared'])
 
-from pathlib import Path
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text(encoding='utf-8')
 
 setuptools.setup(
     name="musiclang-predict",
-    version="1.1.8",
+    version="1.2.0",
     author="Florian GARDIN",
     author_email="fgardin.pro@gmail.com",
     description=("Controllable symbolic music generation with generative AI"
